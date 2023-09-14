8c469c28caf6c9b0420a49aa6e8c8ab4ffc36d20
KillMan <efronofek@gmail.com>
2023-09-14 17:11:58 +0300

    Upload MLO

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,24 +1,4 @@
-fx_version 'bodacious'
+fx_version 'cerulean'
 game 'gta5'
 
-author '5E-Devs'
-description 'Money Wash'
-version '1.0.0'
-
-lua54 'yes'
-
-ui_page 'index.html'
-
-client_scripts {
-    'client.lua',
-    'config.lua'
-}
-
-server_scripts {
-    'server.lua',
-    'config.lua'
-}
-
-files {
-    'index.html'
-}
+this_is_a_map 'yes'
